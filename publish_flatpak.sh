ea15cff75e0849be12dbe30f9f28ddcd270f0180
glosung <154602647+nuedAB@users.noreply.github.com>
2023-12-29 13:51:12 +0100

    Build Flatpak

--- publish_flatpak.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Install autogen tool
-RUN apt-get update -y && \
-    apt-get install -y autogen
-
-# Build your application
-npm install
-npm run build
---
+++ scripts/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Gehe ins Verzeichnis des Projekts
+cd /app
+
+# Installiere Build-Abhängigkeiten (Beispiel: automake, autoconf, libtool)
+flatpak build org.gnome.Sdk/x86_64/3.38 apt install -y automake autoconf libtool
+
+# Konfiguriere das Projekt (passt dies entsprechend den Anforderungen Ihres Projekts an)
+./autogen.sh
+
+# Führe den Build durch
+make
+
+# Installiere das gebaute Programm in das Flatpak-Verzeichnis
+make install DESTDIR=/app
+
+# Führe den Flatpak-Finish-Befehl aus
+flatpak finish --force-clean
